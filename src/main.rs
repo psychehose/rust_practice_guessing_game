11102912c7b0688081ad00adf39d733ae28459e6
psychehose <psychehose@gmail.com>
2023-04-21 08:26:00 +0900

    loop에서 break문 추가

--- src/main.rs
+++ src/main.rs
@@ -55,7 +55,10 @@ fn main() {
         match guess.cmp(&secret_number) {
             Ordering:: Less => println!("Too small"),
             Ordering:: Greater => println!("Too big"),
-            Ordering::Equal => println!("You win"),
+            Ordering::Equal => {
+                println!("You Win");
+                break;
+            }
         }
     }
 }
